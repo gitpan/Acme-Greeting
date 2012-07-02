4d2d7ae057039e58d0ede5d4688c23f23b040230
劉康民 <gugod@gugod.org>
2012-07-02 20:12:30 +0000

    Import of GUGOD/Acme-Greeting-0.0.3 from CPAN.
    gitpan-cpan-distribution: Acme-Greeting
    gitpan-cpan-version:      0.0.3
    gitpan-cpan-path:         GUGOD/Acme-Greeting-0.0.3.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- lib/Acme/Greeting.pm
+++ lib/Acme/Greeting.pm
@@ -1,11 +1,11 @@
 package Acme::Greeting;
-use warnings;
 use strict;
-use 5.006;
+use warnings;
+use v5.12.3;
 use utf8;
 use self;
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 my $last = "";
 
@@ -22,14 +22,21 @@ sub new {
     }
 
     my @greeting = ();
-    open(my $DB, "<:utf8", $realfile);
-    while (<$DB>) {
-        chomp;
-        if ( m/^=item\ (.+)$ /x ) {
-            push @greeting, $1;
+
+    my $DB;
+    open($DB, "<:utf8", $realfile) and do {
+        while (<$DB>) {
+            chomp;
+            if ( m/^=item\ (.+)$ /x ) {
+                push @greeting, $1;
+            }
         }
+        close $DB;
+    };
+
+    if (@greeting == 0) {
+        push @greeting, __PACKAGE__ . ' says hi, $u';
     }
-    close $DB;
 
     my $greeting = $last;
     while ($greeting eq $last) {
